5c2024b180603509c26543d03a5e9cce5ff7be85
GaryOderNichts <12049776+GaryOderNichts@users.noreply.github.com>
2023-10-01 16:43:43 +0200

    Improve buildscripts (#1)

--- cafecompiler/compile_for_host.sh
+++ cafecompiler/compile_for_host.sh
@@ -1,4 +1,3 @@
-# /bin/bash
-cd ..
+#!/usr/bin/env bash
 meson build-host/ -Db_sanitize=none -Dtools=glsl -Dvulkan-drivers= -Dgallium-drivers=r600 -Db_lundef=false -Dglx=disabled -Degl=disabled -Dplatforms= -Dllvm=disabled --buildtype=debug
 ninja -C ./build-host
